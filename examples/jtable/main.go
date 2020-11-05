21117589fbd98dc679e5dda2824d5069af5bf613
cabal29931 <jq@claritasgrc.ai>
2020-11-04 21:34:31 -0800

    Add jtable table plugin to create new osquery tables for the jlogger jconfig plugins.

---
+++ examples/jtable/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"flag"
+
+	"github.com/kolide/osquery-go"
+	"github.com/kolide/osquery-go/plugin/table"
+)
+
+func main() {
+	socket := flag.String("socket", "", "Path to osquery socket file")
+	flag.Parse()
+	if *socket == "" {
+		log.Fatalf(`Usage: %s --socket SOCKET_PATH`, os.Args[0])
+	}
+
+	server, err := osquery.NewExtensionManagerServer("jtable", *socket)
+	if err != nil {
+		log.Fatalf("Error creating extension: %s\n", err)
+	}
+
+	// Create and register a new table plugin with the server.
+	// table.NewPlugin requires the table plugin name,
+	// a slice of Columns and a Generate function.
+	server.RegisterPlugin(table.NewPlugin("jtable", JtableColumns()))
+	if err := server.Run(); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+// FoobarColumns returns the columns that our table will return.
+func JtableColumns() []table.ColumnDefinition {
+	return []table.ColumnDefinition{
+		table.TextColumn("path"),
+		table.TextColumn("txt"),
+		table.TextColumn("hash"),
+	}
+}
