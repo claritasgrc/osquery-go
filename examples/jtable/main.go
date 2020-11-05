602529e905540f3fa4a9f395be034d492d09b77b
cabal29931 <jq@claritasgrc.ai>
2020-11-04 21:39:34 -0800

    Fixes to jtable plugin so things can run

--- examples/jtable/main.go
+++ examples/jtable/main.go
@@ -25,17 +25,29 @@ func main() {
 	// Create and register a new table plugin with the server.
 	// table.NewPlugin requires the table plugin name,
 	// a slice of Columns and a Generate function.
-	server.RegisterPlugin(table.NewPlugin("jtable", JtableColumns()))
+	server.RegisterPlugin(table.NewPlugin("jtable", JtableColumns(), JtableGenerate))
 	if err := server.Run(); err != nil {
 		log.Fatalln(err)
 	}
 }
 
-// FoobarColumns returns the columns that our table will return.
+// JtableColumns returns the columns that our table will return.
 func JtableColumns() []table.ColumnDefinition {
 	return []table.ColumnDefinition{
 		table.TextColumn("path"),
 		table.TextColumn("txt"),
 		table.TextColumn("hash"),
 	}
+}
+
+// JtableGenerate will be called whenever the table is queried. It should return
+// a full table scan.
+func JtableGenerate(ctx context.Context, queryContext table.QueryContext) ([]map[string]string, error) {
+	return []map[string]string{
+		{
+			"path": "/",
+			"txt": "helloworld",
+			"hash": "asdf",
+		},
+	}, nil
 }
